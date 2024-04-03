b1b987026c3a6b9c97366e21a2937ad6d2bead03
Manuel Castellin <manuel@castellinconsulting.com>
2024-04-03 11:04:57 +0200

    general fixes

--- background-task-cancellation/doc.go
+++ background-task-cancellation/doc.go
@@ -6,7 +6,7 @@ package main
 layout: post
 title: "Background Tasks And Cancellation In Go (lang)"
 description: >-
-  In this article we'll learn how to run tasks in the background using Go and implement
+  In this article, we'll learn how to run tasks in the background using Go and implement
   graceful termination.
 date: 2024-03-28
 updated: [cwl:dat curdate]
@@ -16,7 +16,7 @@ image: assets/images/gopher.jpg
 author: "Manuel Castellin"
 permalink: "/golang-background-task-cancellation"
 excerpt: >-
-  In this article we'll learn how to run tasks in the background using Go and implement
+  In this article, we'll learn how to run tasks in the background using Go and implement
   graceful termination.
 ---
 [/cwl:par]
@@ -31,16 +31,16 @@ though long-running one-time operations can be trickier.
 The main issue with one-shot operations is that they are blocking and don't provide any chance to
 bail out prematurely.
 
-#### Example of long running operation
+#### Example of long-running operation
 
 Let's take a look at this implementation:
 [cwl:lnk blockingOp fullSrc=true]
 
-As simple, and useless, the `mockComplexOp` is, it represents a real-world scenario where we need
+As simple as the `mockComplexOp` is, it represents a real-world scenario where we need
 to perform a certain task that is not interruptible. Examples of these tasks are:
 - Complex *oneshot* calculations, like rendering tasks
 - I/O operations, like reading from files or completing a database transaction
-- HTTP interactions with other web-servers
+- HTTP interactions with other web servers
 [/cwl:par]
 
 [cwl:par 20]
@@ -49,7 +49,7 @@ to perform a certain task that is not interruptible. Examples of these tasks are
 
 [cwl:par 20.1]
 The `go` programming language makes it super-easy to run background tasks. In *Golang* they're called
-goroutines and all we need to do to run one is use the `go` keyword, followed by a function call:
+goroutines, and all we need to do to run one is use the `go` keyword, followed by a function call:
 
 ```go
 func main() {
@@ -58,7 +58,7 @@ func main() {
 }
 ```
 
-One thing to notice, when we spawn a background goroutine, the Go runtime will take ownership of that task
+One thing to notice is that when we spawn a background goroutine, the Go runtime will take ownership of that task
 and we can no longer control its state. The sub-routine will **either run to completion or terminate early
 if the main program exits.**
 
@@ -69,8 +69,8 @@ In Go, the only way to interact with a goroutine is by using specific structures
 In this example, `runOnce()` is a simple wrapper function that simply calls the `Do()` method on a `longRunningOp`
 interface and notify the caller as soon as the operation is completed.
 
-Of course there are simpler ways of tracking goroutines completion in go (for instance using `sync.WaitGroup`), but
-it's good to understand how we can do this by ourselves using channels.
+Of course, there are simpler ways of tracking goroutines completion in go (using `sync.WaitGroup`, for example), but
+it's good to understand how we can do this ourselves using channels.
 
 In Go, reading from a channel blocks the program execution until new data is pushed into the channel or the channel
 itself is closed. In the function above, this means that when the `completedCh` is closed, all processes that are
@@ -100,15 +100,15 @@ and attempt a graceful termination for the operation:
 
 [cwl:lnk cancelWithChannel fullSrc=true]
 
-The first thing this function does is **decoupling the long-running operation from the wrapper**. Because the operation is
+The first thing this function does is **decouple the long-running operation from the wrapper**. Because the operation is
 uninterruptible, we can't handle cancellation if the routine is busy running the operation.
 
-We use the `go` keyword to spawn a new sub-routine and delegate the operation execution to it. Because this is an *inner
+We use the `go` keyword to spawn a new sub-routine and delegate the operation execution. Because this is an *inner
 routine*, we also create a dedicated channel to signal the operation completion.
 
-After spawning the sub-routine we introduced the handling logic for cancellation using the `select-case`.
-In this implementation, if the `cancelCh` is closed before the operation sub-routine can complete, the `op.Stop()` method
-is called to request graceful termination.
+After spawning the sub-routine, we introduced the handling logic for cancellation using the `select-case`.
+In this implementation, if the `cancelCh` is closed before the operation sub-routine can complete, we call the `op.Stop()` method
+to request graceful termination.
 
 
 
@@ -155,7 +155,7 @@ In this second example, *the `select` activates the `case` corresponding to the 
 
 ### 3.2. Cancelling an operation with channel
 
-The last thing we need to do, is use the `runOpWithCancelCh` to run a new operation and cancel it before it
+The last thing we need to do is use the `runOpWithCancelCh` to run a new operation and cancel it before it
 can complete:
 
 [cwl:lnk testRunWithCancelCh fullSrc=true]
@@ -165,7 +165,7 @@ can complete:
 [cwl:par 40]
 ## 4. Cancellation Using Context
 
-Since version *1.7* the `context` package has been introduced into Go's *standard library*. It provides **a way to
+Since version *1.7*, the `context` package has been introduced into Go's *standard library*. It provides **a way to
 manage and propagate cancellation signals, deadlines and request-scoped values** across goroutines.
 
 For our purposes, implementing task cancellation using `Context` is not necessarily simpler, though it provides a
@@ -175,7 +175,7 @@ Let's get into it:
 
 [cwl:lnk cancelWithContext  fullSrc=true]
 
-As we can see the implementation is almost identical, except for the cancellation `case` in the handling logic.
+The implementation is almost identical, except for the cancellation `case` in the handling logic.
 To receive the cancellation signal from a `context.Context` all we have to do is read from the `ctx.Done()` channel.
 
 And lastly, here is how we call the function to handle cancellation with context:
